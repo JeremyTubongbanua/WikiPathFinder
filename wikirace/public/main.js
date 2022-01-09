68a26fbbf93ab3e939de7bc3718c9ff214ffc10d
hoomji <selina99tran@gmail.com>
2022-01-08 20:39:01 -0500

    init

---
+++ wikirace/public/main.js
@@ -0,0 +1,53 @@
+//990px
+//672px
+
+//sexy navigation 
+var targetWidth = 992;
+document.addEventListener("DOMContentLoaded", function(){
+
+  el_autohide = document.querySelector('.autohide');
+  el_link_nav = document.querySelectorAll('.link-nav-white');
+  el_autohide.classList.add("fixed-top");
+
+  if(targetWidth >= window.innerWidth){
+    el_autohide.classList.remove("fixed-top");
+  }
+
+  if(el_autohide){
+    var last_scroll_top = 0;
+    window.addEventListener('scroll', function() {
+          let scroll_top = window.scrollY;
+          if(scroll_top == 0){
+            el_autohide.classList.remove('bg-opactiy-10');
+            el_autohide.classList.remove('bg-light');
+
+            el_link_nav.forEach(element => {
+              element.classList.remove('link-nav-black');
+              element.classList.add('link-nav-white');
+            });
+
+          }
+          else if(scroll_top < last_scroll_top) {
+              el_autohide.classList.remove('scrolled-down');
+              el_autohide.classList.add('scrolled-up');
+              el_autohide.classList.add('bg-opactiy-10');
+              el_autohide.classList.add('bg-light');
+              
+              el_link_nav.forEach(element => {
+                element.classList.remove('link-nav-white');
+                element.classList.add('link-nav-black');
+              });
+          }
+          else {
+              el_autohide.classList.remove('scrolled-up');
+              el_autohide.classList.add('scrolled-down');
+          }
+          last_scroll_top = scroll_top;
+    }); 
+    // window.addEventListener
+  }
+  // if
+}); 
+// DOMContentLoaded  end
+
+
--- wikirace/src/App.js
+++ wikirace/src/App.js
@@ -1,8 +1,33 @@
 function App() {
   return (
-    <div className="App">
-      <p>Test</p>
-    </div>
+    <>
+      <form>
+        <div class="form-group">
+          <label for="first-link">First-Link</label>
+          <input type="first-link" class="form-control" id="first-link" placeholder="https://en.wikipedia.org/wiki/My_Teen_Romantic_Comedy_SNAFU_(season_1)"></input>
+        </div>
+        <div class="form-group">
+          <label for="Second-link">Second-Link</label>
+          <input type="second-link" class="form-control" id="second-link" placeholder="https://en.wikipedia.org/wiki/Disguised_Toast"></input>
+        </div>
+        <div class="form-group">
+          <label for="depth">Depth of Clicks</label>
+          <select class="form-control" id="depth">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <label for="output">Wiki-Path</label>
+          <p class="p-1 pb-5 border"></p>
+        </div>
+      </form>
+      <hr></hr>
+      <button type="button" class="btn btn-primary p-3" id="path-find">Path-Find</button>
+    </>
   );
 }
 
