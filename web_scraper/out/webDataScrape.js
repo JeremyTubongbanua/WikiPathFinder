acee3571272709945975c7ec10656e6770f1234f
Christophler <chris.lpher15@gmail.com>
2022-01-10 10:33:58 -0500

    Cleaning up the web scraping code

--- web_scraper/out/webDataScrape.js
+++ web_scraper/out/webDataScrape.js
@@ -1,14 +1,9 @@
 const webScrape = require('./web_scraper');
 
+async function writeJSON(URL) {
 
-async function main() {
+  var linkJSON = { };
   const fs = require('fs');
-  
-  startURL = 'https://en.wikipedia.org/wiki/Professional';
-  
-  var link = {
-    Links: []
-  };
 
   const test = webScrape(startURL).then((result) => {
     var out = result.map(v => v.slice(30))
@@ -17,24 +12,63 @@ async function main() {
       if (err){
           console.log(err);
       } else {
-        link = JSON.parse(data); //now it's an object
-        link.Links.push({[startURL.slice(30)]: [...out]});  // Add the links to the link table
-        json = JSON.stringify(link); //convert it back to json
-        fs.writeFile('linksDB.json', json, function(err, link) {  // write it back 
+        linkJSON = JSON.parse(data); //now it's an object
+        linkJSON[startURL.slice(30)] = [...out]
+        json = JSON.stringify(linkJSON); //convert it back to json
+        fs.writeFile('linksDB.json', json, function(err, linkJSON) {  // write it back 
           if(err) console.log('error', err);
         }); 
     }});
-
-    
-    
   });
+  return;
+}
 
+async function main() {
+  startURL = 'https://en.wikipedia.org/wiki/Subsidiary';
+  console.log(startURL.slice(30));
+  
+  writeJSON(startURL)
   
 }
 
-main();
 
 
 
+function test() {
+  var result = [];
 
+  
+  webScrape('https://en.wikipedia.org/wiki/Subsidiary').then((result) => {
+    console.log(result);
+    console.log("HI", result[0]);
 
+    for (var i = 1; i <= 20; i++) {
+      console.log("HI");
+      var linkJSON = { };
+      const fs = require('fs');
+
+      const test = webScrape(result[i]).then((qoop) => {
+        var out = qoop.map(v => v.slice(30))
+      
+        fs.readFile('wee.json', function readFileCallback(err, data){
+          if (err){
+              console.log(err);
+          } else {
+            linkJSON = JSON.parse(data); //now it's an object
+            linkJSON[qoop[i].slice(30)] = [...out]
+            json = JSON.stringify(linkJSON); //convert it back to json
+            fs.writeFile('wee.json', json, function(err, linkJSON) {  // write it back 
+              if(err) console.log('error', err);
+            }); 
+        }});
+      });
+    }
+
+    webScrape(result[1]).then((eep) => {
+      console.log("eep", eep);
+    });
+  }); 
+
+}
+
+test();
